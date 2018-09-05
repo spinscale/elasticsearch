6e6498f133f342b2e82580bfb5384c3c222ec92f
Alexander Reelsen <alexander@reelsen.net>
2018-09-05 16:04:37 +0200

    fix imports

--- server/src/test/java/org/elasticsearch/common/joda/JavaJodaTimeDuellingTests.java
+++ server/src/test/java/org/elasticsearch/common/joda/JavaJodaTimeDuellingTests.java
@@ -26,12 +26,10 @@ import org.elasticsearch.ElasticsearchParseException;
 import org.elasticsearch.common.time.CompoundDateTimeFormatter;
 >>>>>>> make compound date time formatter return serializable exception
 import org.elasticsearch.common.time.DateFormatters;
-import org.elasticsearch.common.util.LocaleUtils;
 import org.elasticsearch.test.ESTestCase;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 
-import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -40,7 +38,6 @@ import java.util.Locale;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.startsWith;
 
 public class JavaJodaTimeDuellingTests extends ESTestCase {
 
